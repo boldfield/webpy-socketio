67829a056e796efd5e85128e1f93610c2a6f76cc
Brian Oldfield <brian@oldfield.io>
2013-01-17 11:30:36 -0800

    Updates to setup.py, adjusted gevent-socketio version requirement.

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "webpy-socketio",
-    version = "0.0.1",
-    author = "Di SONG",
-    author_email = "songdi19@gmail.com",
-    description = ("A web.py app providing the features required to use "
-                   "websockets with web.py via Socket.IO"),
-    long_description = open("README.rst").read(),
-    url = "https://github.com/songdi/webpy-socketio",
-    py_modules=["webpy_socketio",],
-    install_requires=["gevent-socketio==0.2.1", "web.py>=0.36"],
-    zip_safe = False,
-    include_package_data = True,
-    packages = find_packages(),
-    classifiers = [
+    name="webpy-socketio",
+    version="0.0.2",
+    author="Di SONG, Brian Oldfield",
+    author_email="songdi19@gmail.com, brian@oldfield.io",
+    description=("A web.py app providing the features required to use websockets with web.py via Socket.IO"),
+    long_description=open("README.rst").read(),
+    url="https://github.com/boldfield/webpy-socketio",
+    py_modules=["webpy_socketio", ],
+    install_requires=["gevent-socketio>=0.3.6", "web.py>=0.36"],
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
         "Development Status :: 0 - Beta",
         "Environment :: Web Environment",
         "Framework :: web.py",
